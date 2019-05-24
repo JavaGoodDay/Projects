74aba1094281b298ad57d004126e1933ac363376
JavaGoodDay <callummbailey98@gmail.com>
2019-05-24 10:15:18 +0100

    New Python File

---
+++ First.py
@@ -0,0 +1,14 @@
+Maths =80
+Chemistry=100
+Physics=100
+Total=Maths+Chemistry+Physics
+Per=Total*100/450
+
+print()
+print()
+print("the chemistry result is",Chemistry)
+print("the maths result is",Maths)
+print("the physics result is",Physics)
+print()
+print("Percentage",Per)
+print("the total result is",Total)
